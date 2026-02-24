a3cd38bb180728767453e634791d97b36f8bd254
manishmakode-stps <manish.makode@startappss.com>
2026-02-24 18:35:52 +0530

    updated department and designation module

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import authRoutes from "./modules/auth/auth.routes";
 import employeeRoutes from "./modules/employees/employee.routes";
+import designationRoutes from "./modules/designation/designation.routes";
+import departmentRoutes from "./modules/department/department.routes";
 
 const router: Router = Router();
 
+router.use("/designations", designationRoutes);
 router.use("/login", authRoutes);
 router.use("/employees", employeeRoutes);
+router.use("/departments", departmentRoutes);
 
 export default router;

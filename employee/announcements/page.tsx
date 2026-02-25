0d2c77b57fcaff00a57a1f3968dd869ba872bc40
Jatin Patidar <jatin.patidar@startappss.com>
2026-02-25 16:51:20 +0530

    final structure

---
+++ employee/announcements/page.tsx
@@ -0,0 +1,10 @@
+
+import Announcement from "@/app/EmployeeComponents/Announcements";
+
+export default function AnnouncementRoute() {
+  return (  
+    <div className="flex min-h-screen bg-slate-100">   
+      <Announcement />
+    </div>
+  );
+}

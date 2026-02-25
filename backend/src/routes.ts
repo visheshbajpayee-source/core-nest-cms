a9949195ed8e44251ef2a1507591341552bb9d50
harshitasalvi-sa <harshita.salvi@startappss.com>
2026-02-25 11:52:46 +0530

    Added document module

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import authRoutes from "./modules/auth/auth.routes";
 import employeeRoutes from "./modules/employees/employee.routes";
 import projectRoutes from "./modules/projects/project.routes";
 import taskRoutes from "./modules/tasks/task.routes";
+import documentRoutes from "./modules/documents/documents.routes";
 
 const router: Router = Router();
 
@@ -10,5 +11,6 @@ router.use("/login", authRoutes);
 router.use("/employees", employeeRoutes);
 router.use("/projects", projectRoutes);
 router.use("/tasks", taskRoutes);
+router.use("/documents", documentRoutes);
 
 export default router;

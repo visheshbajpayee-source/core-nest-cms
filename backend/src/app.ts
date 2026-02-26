d9c798fdf97d1a86f7fc835f5180e156e4cc31bf
jainpalash3011 <palash.jain@startappss.com>
2026-02-26 15:40:01 +0530

    changes done

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,12 @@ import { errorHandler, notFoundHandler } from "./common/middlewares/error.middle
 
 const app: Application = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", 'http://localhost:3001'],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 

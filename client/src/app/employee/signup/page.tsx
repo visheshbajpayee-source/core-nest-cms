e64d568518d30fd53f1fdbbb7976db801f26a5b6
jainpalash3011 <palash.jain@startappss.com>
2026-02-27 17:30:48 +0530

    employee folder deleted

--- client/src/app/employee/signup/page.tsx
+++
@@ -1,5 +0,0 @@
-import Signup from '../Login/signup';
-
-export default function SignupPage() {
-  return <Signup />;
-}
--- client/src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Login from './employee/Login/login';
-
-export default function Home() {
-  return <Login />;
-}
